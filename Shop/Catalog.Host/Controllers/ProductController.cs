f0daa9bbe6d7f64cc6b09d0402bd028384d50eed
Larysa Hradyska <l.v.hradyska@gmail.com>
2023-05-31 10:45:55 +0300

    rename catalog class to product

--- Shop/Catalog.Host/Controllers/ProductController.cs
+++ Shop/Catalog.Host/Controllers/ProductController.cs
@@ -4,23 +4,23 @@ namespace Catalog.Host.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class CatalogController : ControllerBase
+    public class ProductController : ControllerBase
     {
         private static readonly string[] Names = new[]
         {
         "Cheese", "Pasta", "Tomato", "Basilic", "Cream", "Wine", "Fish", "Coffee", "Chocolate", "Milk"
         };
-        private readonly ILogger<CatalogController> _logger;
+        private readonly ILogger<ProductController> _logger;
 
-        public CatalogController(ILogger<CatalogController> logger)
+        public ProductController(ILogger<ProductController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetCatalog")]
-        public IEnumerable<Catalog> Get()
+        public IEnumerable<Product> Get()
         {
-            return Enumerable.Range(0, 9).Select(index => new Catalog
+            return Enumerable.Range(0, 9).Select(index => new Product
             {
                 Name = Names[index],
                 Price = Random.Shared.Next(10, 100)/10,
@@ -29,9 +29,9 @@ namespace Catalog.Host.Controllers
             .ToArray();
         }
         [HttpPost(Name = "PostCatalog")]
-        public Catalog Post(string name, int count, double price)
+        public Product Post(string name, int count, double price)
         {
-            Catalog catalog = new Catalog();
+            Product catalog = new Product();
             catalog.Name = name;
             catalog.Price = price;
             catalog.Count = count;
--- Shop/Catalog.Host/Product.cs
+++ Shop/Catalog.Host/Product.cs
@@ -1,6 +1,6 @@
 ﻿namespace Catalog.Host
 {
-    public class Catalog
+    public class Product
     {
         public string? Name { get; set; }
         public int Count { get; set; }
